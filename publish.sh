1f92ac8f655ae4729a0c866866c1466beaa13b47
Haruki Hasegawa <h6a.h4i.0@gmail.com>
2021-02-14 23:11:36 +0900

    Migrate bintrayrelease to maven-publish plugin

--- publish.sh
+++ publish.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -e
 
-./gradlew clean build bintrayUpload \
-    -PbintrayUser="$BINTRAY_USERNAME" \
-    -PbintrayKey="$BINTRAY_KEY" \
-    -PdryRun=false
+./gradlew publishMavenJavaPublicationToMavenCentralRepository
